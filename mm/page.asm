8ea6bf9b0381dfb7239837fc3817ee27e4def766
elitisa <netkom34@gmail.com>
2019-06-15 16:13:42 +0300

    Changed page.s to page.asm

---
+++ mm/page.asm
@@ -0,0 +1,36 @@
+; page.asm contains the low-level page-exception code.
+; he real work is done in mm.c
+;
+; 14.jun.2019
+; Code is rewritten from page.s (GAS) to page.asm (NASM intel syntax) by ISOUX
+
+global page_fault
+extern do_no_page, do_wp_page
+
+page_fault:
+		xchg   [esp],eax
+		push   ecx
+		push   edx
+		push   ds
+		push   es
+		push   fs
+		mov    edx,0x10
+		mov    ds,edx
+		mov    es,edx
+		mov    fs,edx
+		mov    edx,cr2
+		push   edx
+		push   eax
+		test   eax,0x1
+		jne    .1
+		call   do_no_page
+		jmp    .2
+.1:	call   do_wp_page
+.2:	add    esp,0x8
+		pop    fs
+		pop    es
+		pop    ds
+		pop    edx
+		pop    ecx
+		pop    eax
+		iret   
